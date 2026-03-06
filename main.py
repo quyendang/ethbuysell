82ac0148db37712ee0ff4c561a2bdd0f51dcb049
Quyên <11520320@gm.uit.edu.vn>
2026-03-06 13:27:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1214,7 +1214,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)),
+        port=int(os.environ.get("PORT", 8000)),
         reload=True,
         workers=1,
     )
